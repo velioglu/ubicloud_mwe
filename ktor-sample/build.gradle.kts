b36f7f9e0f83d8b96fb42bffa029dcf5c131c194
Emre Şafak <esafak@users.noreply.github.com>
2025-06-08 00:58:47 -0400

    fix kotest

--- ktor-sample/build.gradle.kts
+++ ktor-sample/build.gradle.kts
@@ -1,7 +1,6 @@
-val kotlinVersion: String by project
-
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("io.kotest") version "0.4.11"
     id("io.ktor.plugin") version "3.1.3"
 }
 
@@ -22,13 +21,12 @@ dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1")
     testImplementation("io.kotest:kotest-property:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("org.testcontainers:testcontainers:1.21.1")
 }
 
-//tasks.withType<Test>().configureEach {
-//    useJUnitPlatform()
-//}
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
